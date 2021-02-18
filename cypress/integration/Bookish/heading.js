204b8735aea546d13b5421ea2b130adfa6905e0e
pjcdarker <pjcdarker@gmail.com>
2021-02-18 22:25:39 +0800

    styles: code style

--- cypress/integration/Bookish/heading.js
+++ cypress/integration/Bookish/heading.js
@@ -3,11 +3,11 @@ import { checkAppTitle, gotoApp } from '../../helpers'
 
 Given(`I am a bookish user`, () => {
   console.log('I am a bookish user')
-});
+})
 
 When(`I open the list page`, () => {
   gotoApp()
-});
+})
 
 Then(`I can see the title {string} is showing`, (title) => {
   checkAppTitle(title)
